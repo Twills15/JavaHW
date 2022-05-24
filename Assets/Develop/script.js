705f8f50f81c2b0e23592a570cd89ac28a5cc615
TWrepo <tommywills1538@gmail.com>
2022-05-24 13:56:01 -0400

    added list choices

--- Assets/Develop/script.js
+++ Assets/Develop/script.js
@@ -27,10 +27,29 @@ var userWantsupperCAse = window.confirm("Would you like uppercase letters in the
 
 
 //Made num list
-var NumList
-var SymList =[#$%&*+-.=?]
-var LowerCaselist =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var NumList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var SymList = ["<", ">", ".", "/", "?", "'", "[", "{", "]", "}", "|", "=", "+", "-", "_", ")", "(", "*", "&", "^", "%", "$", "#", "@", "!", "~", "`",]
+var LowerCaselist = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var UpperCaselist = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var ListChoices = []
+
+//prompt list choices
+if (userWantsNums === true) {
+  ListChoices.push(NumList)
+}
+if (userWantsSymbols === true) {
+  ListChoices.push(SymList)
+}
+if (userWantslowerCase === true) {
+  ListChoices.push(LowerCaselist)
+}
+if (userWantsupperCAse === true) {
+  ListChoices.push(UpperCaselist)
+}
+console.log(ListChoices)
+
+
 
 
 
